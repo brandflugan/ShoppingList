7374bbcb206f91028e3346883f3cb856e7e4e9d4
charlarn <cbfauxpas@gmail.com>
2017-02-20 14:29:15 +0100

    added file upload page

---
+++ ShoppingList/ShoppingList/Controllers/MatkrisController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingList.Controllers
+{
+    public class MatkrisController : Controller
+    {
+        // GET: Matkris
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ValidateFileUpload()
+        {
+            if (Request.Files.Count > 0)
+            {
+                var file = Request.Files[0];
+
+                if (file != null && file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    List<string> lines = new List<string>();
+
+                    using (StreamReader reader = new StreamReader(file.InputStream, Encoding.UTF8))
+                    {
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            lines.Add(line);
+                            Debug.WriteLine(line);
+                        }
+                    }
+                    //hello
+                }
+            }
+            return null;
+            //hello
+        }
+    }
+}
